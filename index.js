e496ade72882d3ee4644cfb638b441b82b259930
Maxime Chourré <maxou3153@gmail.com>
2018-04-10 21:02:15 +0200

    1.0.1

--- index.js
+++ index.js
@@ -7,23 +7,36 @@ const prefix = "!";
 client.login(process.env.token);
 client.on("ready", () => {
     console.log('bot prêt');
-} );
+});
 
-client.on('message', message => {
+client.on('message', async message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    if(message.content.startsWith("Bonjour") || message.content.startsWith("bonjour")){
+    if (message.content.startsWith("Bonjour") || message.content.startsWith("bonjour")) {
         message.reply("salut, comment ça va?");
         console.log('le bot dit bonjour et demande comment ça va?');
     }
 
-    if(message.content.startsWith === "oui et toi"){
+    if (message.content.startsWith("oui et toi")) {
         message.reply("je te remercie je vais toujour bien, tu peut allez voté sur https://gta.top-serveurs.net/maad-city-rp ainsi que vous @everyone :heart:");
         console.log('le bot remerci et donne le lien du vote');
-}
+    }
 
-    if(command === "vote"){
+    if (command === "vote") {
         message.reply("@everyone Goo voteerrrr :heart: https://gta.top-serveurs.net/maad-city-rp");
         console.log('le bot donne le lien du vote');
     }
+    if (command === 'purge') {
+        const deleteCount = parseInt(args[0], 10);
+        if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+            return message.reply("Merci de donner un nombre de 2 à 100 messages à supprimer.");
+
+        // So we get our messages, and delete them. Simple enough, right?
+        const fetched = await message.channel.fetchMessages({
+            limit: deleteCount
+        });
+        message.channel.bulkDelete(fetched)
+            .catch(error => message.reply(`Erreur: ${error}`));
+    }
+
 });
