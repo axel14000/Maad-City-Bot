b70f494c2c28090555a991428699ded2c44bc280
axel14000 <axelfoulon@orange.fr>
2018-04-26 12:16:21 +0200

    1.0.1

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ client.on('message', async message => {
     const command = args.shift().toLowerCase();
 
     client.on("guildMemberAdd", function(member) {
-        member.guild.channels.find("name", "arrivé-et-depar").sendMessage(" Bienvenue sur mon serveur " + member.toString());
+        member.guild.channels.find("name", "arrivé et depar").sendMessage(" Bienvenue sur mon serveur " + member.toString());
         member.addRole(member.guild.roles.find("name", "Test"));
     });
     
     client.on("guildMemberRemove", function(member) {
-        member.guild.channels.find("name", "arrivé-et-depar").sendMessage( member.toString() + "Nous a quitté ! ");
+        member.guild.channels.find("name", "arrivé et depar").sendMessage( member.toString() + "Nous a quitté ! ");
     });
 
     if (message.content.startsWith("Bonjour") || message.content.startsWith("bonjour") ||message.content.startsWith("Salut") || message.content.startsWith("salut") ){
